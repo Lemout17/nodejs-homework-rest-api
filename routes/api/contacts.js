d24880ee74115dd36656fd5f6456a9b516e55d39
Vlad Safin <djutsu17@gmail.com>
2021-08-24 16:17:27 +0300

    add functions in model/index.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,31 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
 
-router.get("/", async (req, res, next) => {
-  res.json({ message: "Homework done!" })
+const contactsOperations = require('../../model/index')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const contacts = await contactsOperations.listContacts()
+    res.json({ contacts })
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" })
+router.get('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
 })
 
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" })
+router.post('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
 })
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" })
+router.delete('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
 })
 
-router.patch("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" })
+router.patch('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
 })
 
 module.exports = router
