684280ff6bdff082fbc2db9810a38b2e9b198514
Vlad Safin <djutsu17@gmail.com>
2021-09-18 13:50:43 +0300

    fix

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,21 +1,16 @@
 const gravatar = require('gravatar')
-
 const { Conflict } = require('http-errors')
-
 const { User } = require('../../models')
 
 const signup = async (req, res) => {
   const { email, password } = req.body
-
   const user = await User.findOne({ email })
+
   if (user) {
     throw new Conflict('Email in use')
   }
 
   const defaultImage = gravatar.url(email, { s: '250' }, true)
-
-  console.log(defaultImage)
-
   const newUser = new User({ email, avatarURL: defaultImage })
   newUser.setPassword(password)
   await newUser.save()
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,35 +1,27 @@
 const express = require('express')
 const { userJoiSchema } = require('../../models/user')
-
 const {
   validation,
   tryCatchWrapper,
   authenticate,
   upload,
 } = require('../../middlewares')
-
 const { auth: ctrl } = require('../../controllers')
-
 const router = express.Router()
-
 const userValidationMiddleware = validation(userJoiSchema)
 
 router.post('/signup', userValidationMiddleware, tryCatchWrapper(ctrl.register))
-
 router.post('/login', userValidationMiddleware, tryCatchWrapper(ctrl.login))
-
 router.get(
   '/logout',
   tryCatchWrapper(authenticate),
   tryCatchWrapper(ctrl.logout)
 )
-
 router.get(
   '/current',
   tryCatchWrapper(authenticate),
   tryCatchWrapper(ctrl.current)
 )
-
 router.patch(
   '/avatars',
   tryCatchWrapper(authenticate),
